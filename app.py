bd4a7e2fa24a8d912bc2cf1031e2f9df6e873bee
Rewich_1 <jean-benoit.charbonnaud@efrei.net>
2023-01-30 18:45:14 +0100

    passe de txt à cookie

--- app.py
+++ app.py
@@ -3,16 +3,22 @@ import numpy as np
 import pandas as pd
 import json
 import time
+from streamlit_cookies_manager import EncryptedCookieManager
+import os
 
-historical = open("historical.txt", "r")
-#st.write(historical)
-historical = str(historical.read())
-#st.write(historical)
-#st.write(len(historical))
-if len(historical) == 0:
+# This should be on top of your script
+cookies = EncryptedCookieManager(
+    prefix="ktosiek/streamlit-cookies-manager/",
+    password=os.environ.get("COOKIES_PASSWORD", "lest go"),
+)
+
+if not cookies.ready():
+    st.stop()
+try :
+    historical = json.loads(cookies['historical'].replace("'", '"'))
+except:
+    cookies['historical'] = "[]"
     historical = []
-else :
-    historical = json.loads(historical.replace("'", '"'))
 
 def answer(text):
     return "yes i am bob"
@@ -24,8 +30,6 @@ tab1, tab2 = st.tabs(["tchat", "tuto"])
 with tab1:
     st.header('tchat ')
 
-
-
     st.write('<style>[data-testid="stVerticalBlock"] > [style*="flex-direction: column;"] > [data-testid="stVerticalBlock"] {max-height: 400px;overflow: auto;flex-direction:column-reverse;} </style>', unsafe_allow_html=True)
     container = st.container()
 
@@ -35,45 +39,53 @@ with tab1:
         historical.append({'user':1,'text':question})
         historical.append({'user': 0, 'text': answer(question)})
 
-        historical.reverse()
+    historical.reverse()
 
-        with container:
-            for i in range(len(historical)):
-                if historical[i]["user"]==1:
-                    col1, col2,col3, col4 = st.columns([3,20,20,3])
-
-                    col1.image("homme.png")
-                    col2.info(historical[i]['text'])
+    with container:
+        for i in range(len(historical)):
+            if historical[i]["user"]==0:
+                col1, col2,col3, col4 = st.columns([3,20,20,3])
+                if i == 0:
+                    placeholder = st.empty()
+                    with placeholder:
+                        for seconds in range(1):
+                            st.write(f"⏳")
+                            time.sleep(1)
+                    placeholder.empty()
+                    col1.image("fille.png")
+                    col2.success(historical[i]['text'])
                 else:
+                    col1.image("fille.png")
+                    col2.warning(historical[i]['text'])
 
-                    col1, col2,col3, col4 = st.columns([3,20,20,3])
-                    if i == 0:
-                        placeholder = st.empty()
-                        with placeholder:
-                            for seconds in range(1):
-                                st.write(f"⏳")
-                                time.sleep(1)
-                        placeholder.empty()
-                        col3.success(historical[i]["text"])
-                        col4.image("fille.png")
+            else:
 
-                    else:
-                        col3.warning(historical[i]["text"])
-                        col4.image("fille.png")
+                col1, col2,col3, col4 = st.columns([3,20,20,3])
+                col3.info(historical[i]["text"])
+                col4.image("homme.png")
 
-        historical.reverse()
-        with open("historical.txt", "w") as file:
-            file.write(str(historical))
+
+    historical.reverse()
+    cookies['historical'] = str(historical)
+    cookies.save()
+   
 
 
 with tab2:
     st.header('documentation')
-    with st.expander("doc1"):
+    recherche = st.text_input(value="", label="recherche")
+    with st.expander("information sur les personnes politiques"):
+        st.subheader("le parcours d'une personne politique")
+        st.write('doc1')
+        st.subheader("sa participation a certain débat")
         st.write('doc1')
 
-    with st.expander("doc2"):
-        st.write('doc2')
-        st.write('doc3')
+
+    with st.expander("information sur les séances parlementaires"):
+        st.subheader("des information en fonction d'une période bien précise")
+        st.write('doc1')
+        st.subheader("aprofondir certain sujet")
+        st.write('doc1')
 
 
 
