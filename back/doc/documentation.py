8a63ac51848862b6eab050e3970818608cc7e994
Rewich_1 <jean-benoit.charbonnaud@efrei.net>
2023-02-25 12:49:52 +0100

    add select dossier
    add select dossier

--- back/doc/documentation.py
+++ back/doc/documentation.py
@@ -6,13 +6,17 @@ import pandas as pd
 def documentation():
     with st.expander("information sur les personnes politiques"):
         st.subheader("le parcours d'une personne politique")
-        st.write('doc1')
-        st.subheader("sa participation a certain débat")
-        st.write('doc1')
+        st.write('qui sont les députés  de paris ?')
+        st.subheader("demander des information sur une personne politique")
+        st.write('qui est david amiel ?')
 
+    with st.expander("information sur le planing politique"):
+        st.subheader("en savoir plus sur l'agenda de l'assemblée nationale")
+        st.write("quel est l'agenda de l'assemblé nationale ?")
 
-    with st.expander("information sur les séances parlementaires"):
-        st.subheader("des information en fonction d'une période bien précise")
-        st.write('doc1')
-        st.subheader("aprofondir certain sujet")
-        st.write('doc1')
+    with st.expander("information sur les dossiers traités à l'Assemblée"):
+        st.subheader("savoir les dossier traité à l'assemblée")
+        st.write('donne moi les dossier traité à l\'assemblée ce mois ci')
+        st.subheader("approfondir sur un dossier")
+        st.write('dis moi en plus sur le dossier "Projet de loi de financement rectificative de la sécurité sociale pour 2023"')
+
